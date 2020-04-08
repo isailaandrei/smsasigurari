ac4bcd55611fb4ef2fe31981ae46537465b75c5a
isailaandreii <isailaandrei.i@gmail.com>
2020-04-08 23:03:00 +0100

    Change django secret key and add it as environment variable

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7ks=*w+lq#7_@td&#kbd5n#_58dd40@s3sj1&^y$s$6o0b752#'
+SECRET_KEY = os.environ['DJANGO_SESCRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DJANGO_DEBUG']
