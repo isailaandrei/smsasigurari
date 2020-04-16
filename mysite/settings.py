fe28a583b3e6070affe408ab6b063ce1f7b4d642
isailaandreii <isailaandrei.i@gmail.com>
2020-04-16 11:52:34 +0100

    www.rca.online :)

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ['DJANGO_DEBUG'] in (True, 'True'))
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'smsasigurari.herokuapp.com', 'rca.online']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'smsasigurari.herokuapp.com', 'rca.online', 'www.rca.online']
 
 
 # Application definition
